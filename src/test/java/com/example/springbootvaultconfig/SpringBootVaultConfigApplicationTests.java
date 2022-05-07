c765471760b7fa79c09431e0ffa2455e8e804dc8
Atul.Gaurav <aatul.gaurav@gmail.com>
2022-05-08 01:32:45 +0530

    Initial commit

---
+++ src/test/java/com/example/springbootvaultconfig/SpringBootVaultConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootvaultconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVaultConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
